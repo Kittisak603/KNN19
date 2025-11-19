2dd0f5b52cf4119d18dbbe30ec0b123af172c056
Kittisak603 <664230009@webmail.npru.ac.th>
2025-11-19 15:40:37 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,24 +3,29 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 st.title('KNN Classification - Iris Dataset')
-st.header('น้องกิตตฺศักดิ์ เองครับ')
+st.header('น้องกิตติศักดิ์ เองครับ')
 
-# แสดงรูปหลัก
-st.image("./img/1704165478897.jpg")
+# ใช้ os เพื่อป้องกันปัญหา path ไม่เจอ
+BASE = os.path.dirname(__file__)
 
-# แสดงรูปทั้ง 3 แบบ แบ่งเป็นคอลัมน์
+# รูปหลัก
+st.image(os.path.join(BASE, "img/1704165478897.jpg"))
+
+# แสดงรูป 3 แบบใน 3 คอลัมน์
 col1, col2, col3 = st.columns(3)
 
 with col1:
     st.subheader("Versicolor")
-    st.image("./img/iris1.jpg")
+    st.image(os.path.join(BASE, "img/iris1.jpg"))
 
 with col2:
     st.subheader("Virginica")
-    st.image("./img/iris2.jpg")
+    st.image(os.path.join(BASE, "img/iris2.jpg"))
 
 with col3:
     st.subheader("Setosa")
-    st.image("./img/iris3.jpg")
+    st.image(os.path.join(BASE, "img/iris3.jpg"))
+
